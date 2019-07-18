5eb8e0d35b3f3917f5c2a7d22051c9c61ccb3448
norest <orestisef@gmail.com>
2019-07-18 22:33:56 +0300

    setup memoryCard game
    -setup timer
    -setup rounds
    -setup lifesCount

--- login_signup/Games/script.js
+++ login_signup/Games/script.js
@@ -3,9 +3,17 @@ const cards = document.querySelectorAll('.memory-card');
 let hasFlippedCard = false;
 let lockBoard = false;
 let firstCard, secondCard;
+let finishGame = false;
+let countDisableCards = 0;
+let round = 1;
+let count = 0;
+let score = 0;
+let life = 3;
+let wins = 0;
 
 function flipCard() {
   if (lockBoard) return;
+  count++;
   if (this === firstCard) return;
 
   this.classList.add('flip');
@@ -31,10 +39,16 @@ function checkForMatch() {
 }
 
 function disableCards() {
+  countDisableCards++;
   firstCard.removeEventListener('click', flipCard);
   secondCard.removeEventListener('click', flipCard);
-
-  resetBoard();
+  if (countDisableCards == 6){
+    ++life;
+    cards.forEach(card => setTimeout(()=> {card.classList.remove('flip')}, 1600));
+    //cards.forEach( card => card.removeEventListener('click', flipCard));
+  }else{
+    resetBoard();
+  }
 }
 
 function unflipCards() {
@@ -53,11 +67,60 @@ function resetBoard() {
   [firstCard, secondCard] = [null, null];
 }
 
-(function shuffle(){
+function shuffle(){
   cards.forEach( card => {
     let randomPos = Math.floor(Math.random() * 12);
     card.style.order = randomPos;
   });
-})();
+};
+
+function countDown(secs, elem1) {
+  var element = document.getElementById(elem1);
+  document.getElementById('round').innerHTML= 'Γύρος: '+round;
+  document.getElementById('count').innerHTML='Γυρίσματα: '+count;
+  document.getElementById('life').innerHTML= 'Ζωές : '+life;
+  element.innerHTML = "00:"+secs;
+  if(secs<10) {
+    element.innerHTML = "00:0"+secs;
+  }
+  countDownLife(secs);
+  if(secs < 0) {
+    clearTimeout(timer);
+    element.innerHTML = 'Γύρος '+(++round);
+    resetRound();
+    return;
+  }
+  secs--;
+  var timer = setTimeout('countDown('+secs+',"'+elem1+'")',1000);
+}
+
+
+function resetRound() {
+  cards.forEach( card => card.removeEventListener('click', flipCard));
+  cards.forEach(card => card.classList.add('flip'));
+  cards.forEach(card => card.classList.remove('flip'));
+  cards.forEach( card => card.addEventListener('click', flipCard));
+  resetBoard();
+  setTimeout('shuffle()',1000);
+  count = 0;
+  countDisableCards = 0;
+  setTimeout('countDown(28, "status")',3000);
+  cards.forEach( card => card.removeEventListener('click', flipCard));
+  cards.forEach( card => setTimeout(()=> {card.addEventListener('click', flipCard)},3000));
+}
+
+
+function countDownLife(secs) {
+  if ((countDisableCards!=6) && (secs == 0)){
+    --life;
+    if (life == 0){
+      cards.forEach( card => card.removeEventListener('click', flipCard));
+      cards.forEach(card => card.classList.add('flip'));
+      document.location.reload();
+    }
+  }
+}
+
 
 cards.forEach( card => card.addEventListener('click', flipCard));
+shuffle();
